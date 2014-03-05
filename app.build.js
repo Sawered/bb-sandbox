565e226cf80796021ee85aadd574601634fc6400
Sawered <skoryukin@medee.ru>
2014-03-05 16:52:38 +0400

    getting all

---
+++ app.build.js
@@ -0,0 +1,12 @@
+({
+    appDir: "./js",
+    baseUrl: ".",
+    mainConfigFile: "js/config.js",
+    dir: "./js-build",
+    modules: [
+        {
+            name: "main"
+        }
+    ],
+    //"out": "js/main-built.js"
+})
---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Start the main app logic.
+require([
+        'jquery'
+
+],
+function(
+$
+) {
+
+    console.log('Main loaded');
+});
